4667124763ceb9b1d33f12fe56ffe0c9db63fcf3
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-13 12:12:14 +0530

    hystrix application

---
+++ src/main/java/com/weatherApp/G1_weatherapp/G1WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.weatherApp.G1_weatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G1WeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G1WeatherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weatherApp/G1_weatherapp/G1WeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherApp.G1_weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1WeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
